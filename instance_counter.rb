7521c2f8b759d5baa951965bb346eb5c7ff7cafa
hunter298 <anton-volianskyi@ukr.net>
2020-03-07 19:31:37 +0200

    with modules

--- instance_counter.rb
+++ instance_counter.rb
@@ -9,17 +9,12 @@ module InstanceCounter
 
   module ClassMethods
     attr_accessor :instances
-
-    def instances
-      return @instances unless @instances.class == NilClass
-      0
-    end
   end
 
   module InstanceMethods
     def register_instance
       if self.class.instances != nil
-        self.class.instances = self.class.instances + 1
+        self.class.instances += 1
       else
         self.class.instances = 1
       end
